bdabe4bbdf4ec8783b78920dd89d567df31041c4
Erick Riaño <123erickrh@gmail.com>
2024-06-03 17:50:58 -0500

    Add linter

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+import { ButtonGroup, Button, Box, Container } from '@mui/material';
 
 export default function Home() {
   return (
-    <main>
-      <p>hello world</p>
+    <main className='bg-background-beige'>
+      <Container>
+        <Box>
+          <ButtonGroup
+            variant='text'
+            aria-label='Basic button group'
+            size='small'
+            orientation='horizontal'
+          >
+            <Button>Home</Button>
+            <Button>About</Button>
+            <Button>Technologies</Button>
+            <Button>Projects</Button>
+            <Button>Contact</Button>
+          </ButtonGroup>
+        </Box>
+      </Container>
     </main>
   );
 }
