ec2f9df04ccc495f5f34c62f82f41a8845336de9
Erick Riaño <123erickrh@gmail.com>
2024-06-08 14:38:59 -0500

    Start tech stack section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { transform } from 'next/dist/build/swc';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
@@ -12,6 +13,19 @@ const config: Config = {
         'background-beige': '#F0F0E8',
         'custom-black': '#181818',
         'grayish-yellow': '#f0f0e8',
+        taupe: '#807b6b',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)',
+          },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
       },
     },
   },
